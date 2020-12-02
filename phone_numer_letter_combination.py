26f35f0e1d5fd745117da3759ca755f223060a8b
krishnx <krishnashoot@gmail.com>
2020-12-02 20:58:40 +0530

    Create phone_numer_letter_combination.py

---
+++ phone_numer_letter_combination.py
@@ -0,0 +1,22 @@
+from itertools import product
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        maps = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w",  "x", "y", "z"]
+        }
+        arr = [maps[i] for i in digits]
+        return ["".join(a) for a in product(*arr)]
