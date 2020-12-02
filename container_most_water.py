7d24f8696be4b1dc880e5fdc170ba0d459052ac9
krishnx <krishnashoot@gmail.com>
2020-12-02 20:54:43 +0530

    Create container_most_water.py

---
+++ container_most_water.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height or len(height) < 2:
+            return 0
+        
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        while left < right:
+            
+            if height[left] < height[right]:
+                max_area = max(max_area, height[left] * (right-left))
+                left += 1
+            else:
+                max_area = max(max_area, height[right] * (right-left))
+                right -= 1
+            
+        
+        return max_area
+        
